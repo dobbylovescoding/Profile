e80f18d18c7b5752d8c14a20af7ceb53b065e3eb
Conor Howard <howard.conor@gmail.com>
2023-01-03 14:16:15 +1300

    Contact page updates

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,29 @@
+import { useRef } from "react";
 import styles from "./Contact.module.css";
+import emailjs from "@emailjs/browser";
 
 export default function Contact() {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_p2genat",
+        "template_51pbs1u",
+        form.current,
+        "9TDoLV3m4itARDMW3"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
   return (
     <div className={styles.cont}>
       <div className={styles.cont1}>
@@ -10,33 +33,38 @@ export default function Contact() {
           but I am also open to any oppurtunity that will allow me to develop my
           programming skills further.
         </p>
-        <form className={styles.form}>
-          <input type="text" className={styles.name} placeholder="Name"></input>
+        <form ref={form} onSubmit={sendEmail} className={styles.form}>
           <input
+            className={styles.name}
             type="text"
+            placeholder="Name"
+            name="user_name"
+            required
+          />
+          <input
             className={styles.email}
+            type="email"
             placeholder="Email"
-          ></input>
-          <br />
+            name="user_email"
+            required
+          />
           <input
-            type="text"
             className={styles.subject}
-            placeholder="Subject"
-          ></input>
-          <br />
-          <textarea
             type="text"
-            rows="10"
-            cols="20"
-            placeholder="Message"
+            placeholder="Subject"
+            name="subject"
+            required
+          />
+          <textarea
             className={styles.message}
-          ></textarea>
-          <br />
-          <input
-            className={styles.submit}
-            type="submit"
-            value="Submit!"
-          ></input>
+            placeholder="Message"
+            name="message"
+            cols="30"
+            rows="10"
+          />
+          <button className={styles.submit} type="submit">
+            Submit
+          </button>
         </form>
       </div>
       <div className={styles.cont2}></div>
